8c9840bf7dc34ed524da3d09b03e754345efc096
shino-taira <shiiinop0401@gmail.com>
2023-07-30 18:03:40 +0900

    fix test file

--- serverspec/spec/lalala/sample_spec.rb
+++ serverspec/spec/lalala/sample_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
-listen_port = 8080
+#80番ポート(Nginx)を指定
+listen_port = 80
 
+#Nginxがインストールされているか確認
 describe package('nginx') do
   it { should be_installed }
 end
 
+#80番ポートをListenしているか確認
 describe port(listen_port) do
   it { should be_listening }
 end
 
+#HTTPアクセスで200/OKが返ってくるか確認
 describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
